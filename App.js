86046b215e6e0386ea85859c67708061197aca52
Levladik <vladislavtyshenko@yandex.ru>
2024-05-15 21:54:15 +0400

    add two inputs and switch components

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, SafeAreaView, TextInput, Switch } from 'react-native';
+import { useState } from'react';
 
 export default function App() {
+
+  const [name, setName] = useState('');
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <TextInput style={styles.input} value={name} onChangeText={setName}/>
+      <Text style={styles.text}>My name is {name}</Text>
+      <TextInput style={[styles.input, styles.multilineText]} multiline placeholder='message'/>
+      <View style={styles.switchContainer}>
+        <Text style={styles.text}>Dark Mode</Text>
+        <Switch 
+          value={isDarkMode}
+          onValueChange={() => setIsDarkMode((previousState) => !previousState)}
+          trackColor={{false: '#3e3e3e', true: '#81b0ff'}}
+          thumbColor={isDarkMode ? '#f5dd4b' : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"/>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +29,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
   },
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: 'gray',
+    borderRadius: 10,
+    backgroundColor: 'white'
+  },
+
+  text: {
+    fontSize: 30,
+    padding: 10,
+  },
+
+  multilineText: {
+    height: 100,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: 'gray',
+    borderRadius: 10,
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    justifyContent:'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+  }
 });
